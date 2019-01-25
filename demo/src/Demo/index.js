2afa8e7e2a0253a7bce37427bfdeb78ceb9e1430
余珩 <1281005689@qq.com>
2019-01-25 18:36:37 +0800

    [core] Initial project

---
+++ demo/src/Demo/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import demo from 'react-markdownElement';
+
+/**
+ * Export a form call api with post method
+ */
+@hot(module)
+class Demo extends React.Component {
+  text = demo();
+
+  /**
+   * Render Something.
+   * @return {Node}
+   */
+  render() {
+    return <div>{this.text}</div>;
+  }
+}
+
+export default Demo;
