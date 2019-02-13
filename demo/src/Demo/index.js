421e82531b8a0f306c4dc65827f2aa0a4ea6d32d
余珩 <1281005689@qq.com>
2019-02-13 14:40:34 +0800

    [Demo] Optimize demo

--- demo/src/Demo/index.js
+++ demo/src/Demo/index.js
@@ -2,25 +2,17 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import PrismMarked from 'react-prism-marked';
 
-const code = `
-\`\`\`jsx
-import React from 'react';
-
-class Demo extends React.PureComponent {
-  render() {
-    return 'demo';
-  }
-}
-
-export default Demo;
-\`\`\`
-`;
-
 @hot(module)
 class Demo extends React.Component {
   render() {
+    const raw = preval`
+      module.exports = require('fs').readFileSync(require.resolve('./index'), 'utf8')
+    `;
+
     return (
-      <PrismMarked text={code} />
+      <PrismMarked
+        text={`\`\`\`jsx\n${raw}\n\`\`\``}
+      />
     );
   }
 }
